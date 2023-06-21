7afb81afe75e7a6f6eb9ad96a261b4531eb494c7
Michael Tran <solidwater969@gmail.com>
2023-06-21 08:55:27 -0400

    feature | add chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, ElementRef, ViewChild } from '@angular/core'
 
 @Component({ selector: 'app-root', templateUrl: './app.component.html' })
-export class AppComponent {}
+export class AppComponent {
+  @ViewChild('scroll') scroll!: ElementRef
+  messages: string[] = []
+
+  ngAfterViewChecked() {
+    if (!this.scroll) return
+    this.scroll.nativeElement.scrollTop = this.scroll.nativeElement.scrollHeight
+  }
+
+  ngOnInit() {
+    if (!this.scroll) return
+    this.scroll.nativeElement.scrollTop = this.scroll.nativeElement.scrollHeight
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -6,7 +7,7 @@ import { AppComponent } from './app.component'
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule],
+  imports: [AppRoutingModule, BrowserModule, FormsModule],
   providers: []
 })
 export class AppModule {}
