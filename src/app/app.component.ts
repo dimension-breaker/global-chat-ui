1a0ecb00c2fe9f3c8f7d7f20ba6b9ace140c2f46
Michael Tran <solidwater969@gmail.com>
2023-08-15 09:21:17 -0400

    refactor | flex auto to h-full

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,9 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   }
 </style>
 <div class="flex flex-rol gap-2 h-screen p-2" [ngStyle]="{ flex: '1 1 auto' }">
-  <div class="w-2 sm:w-auto z-10">
+  <div class="sm:w-auto w-4 z-10">
     <div class="bg-white border-2 duration-200 flex flex-col h-full overflow-hidden" (click)="isExpanded = !isExpanded" [ngStyle]="{ 'max-width': isExpanded ? '256px' : '16px', 'min-width': isExpanded ? '256px' : '16px' }">
-      <div [ngStyle]="{ flex: '1 1 auto' }">
+      <div class="duration-200 h-full" [ngStyle]="{ opacity: isExpanded ? 1 : 0 }">
         <div class="font-bold p-2">Users</div>
         <div class="hover:bg-gray-200 p-2" *ngFor="let user of ['bob', 'sally', 'doll']">{{ user }}</div>
       </div>
@@ -18,7 +18,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   </div>
 
   <div class="flex flex-col gap-2 h-full min-w-0" [ngStyle]="{ flex: '1 1 auto' }">
-    <div class="flex flex-col gap-2 overflow-auto" [ngStyle]="{ flex: '1 1 auto' }" #scroll>
+    <div class="flex flex-col gap-2 h-full overflow-auto" #scroll>
       <div class="border-2 flex flex-col gap-2 p-2 w-fit" *ngFor="let message of messages; let i = index" [ngStyle]="{ marginTop: i === 0 ? 'auto' : undefined }">
         <div class="font-bold">bob</div>
         <div>{{ message }}</div>
