c212ab09d8a057d027d3014b26b45f8e76873d6d
Michael Tran <solidwater969@gmail.com>
2023-08-31 15:25:12 -0400

    feature | add text replacement list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({ selector: 'app-root', template: `
-<style>
-  ::-webkit-scrollbar {
-    width: 0;
-  }
-</style>
 <div class="flex flex-rol gap-2 h-screen p-2" [ngStyle]="{ flex: '1 1 auto' }">
   <div class="sm:w-auto w-4 z-10">
     <div class="bg-white border-2 duration-200 flex flex-col h-full overflow-hidden" (click)="isExpanded = !isExpanded" [ngStyle]="{ 'max-width': isExpanded ? '256px' : '16px', 'min-width': isExpanded ? '256px' : '16px' }">
-      <div class="duration-200 h-full" [ngStyle]="{ opacity: isExpanded ? 1 : 0 }">
+      <div class="cursor-pointer duration-200 h-full" [ngStyle]="{ opacity: isExpanded ? 1 : 0 }">
         <div class="font-bold p-2">Users</div>
         <div class="hover:bg-gray-200 p-2" *ngFor="let user of ['bob', 'sally', 'doll']">{{ user }}</div>
       </div>
--- src/app/settings.component.ts
+++ src/app/settings.component.ts
@@ -1,20 +1,26 @@
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
-import { faGear, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
+import { faGear, faLongArrowAltRight, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({ selector: 'settings', template: `
 <div class="p-2" (click)="$event.stopPropagation()">
-  <fa-icon class="block hover:bg-gray-200 p-2 w-fit" (click)="showDialog = !showDialog" [icon]="faGear" [ngStyle]="{ lineHeight: 0 }"/>
+  <fa-icon class="block cursor-pointer hover:bg-gray-200 p-2 w-fit" (click)="showDialog = !showDialog" [icon]="faGear" [ngStyle]="{ lineHeight: 0 }"/>
 </div>
 <dialog class="border-2 h-1/2 inset-2/4 p-0 w-1/2" (click)="$event.stopPropagation()" [ngStyle]="{ translate: '-50%' }" [open]="showDialog" #settings>
   <div class="flex flex-row h-full">
     <div [ngStyle]="{ flex: '0 1 25%' }">
-      <div class="bg-gray-100 hover:bg-gray-200 p-2">Text Replacements</div>
+      <div class="bg-gray-100 cursor-pointer hover:bg-gray-200 p-2">Text Replacements</div>
     </div>
     <div class="border-r-2"></div>
-    <div class="p-2" [ngStyle]="{ flex: '0 1 75%' }">
-      <input class="border-2 focus:outline-none h-12 p-2">
-      <fa-icon class="p-2 w-fit" [icon]="faLongArrowAltRight" [ngStyle]="{ lineHeight: 0 }"/>
-      <input class="border-2 focus:outline-none h-12 p-2">
+    <div class="flex flex-col gap-2 p-2" [ngStyle]="{ flex: '0 1 75%' }">
+      <div class="flex flex-rol items-center justify-between">
+        <strong>Text Replacements</strong>
+        <fa-icon class="block cursor-pointer hover:bg-gray-200 p-2 w-fit" [icon]="faPlus" [ngStyle]="{ lineHeight: 0 }"/>
+      </div>
+      <div class="overflow-auto" [ngStyle]="{ flex: '1 1 auto' }">
+        <div class="cursor-pointer hover:bg-gray-200 p-2" *ngFor="let textReplacement of [{ replacement: 'my bad', text: 'mb' }, { replacement: 'shut the firetruck up', text: 'stfu' }, { replacement: 'no', text: 'yes' }]">
+          {{textReplacement.text}} -> {{textReplacement.replacement}}
+        </div>
+      </div>
     </div>
   </div>
 </dialog>
@@ -22,6 +28,7 @@ import { faGear, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
 export class SettingsComponent {
   faGear = faGear
   faLongArrowAltRight = faLongArrowAltRight
+  faPlus = faPlus
   @ViewChild('settings') settings!: ElementRef
   showDialog = false
 
